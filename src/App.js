aad78bacea9b84b389c1cc29750e5f67bde6ee81
DVS-675 <dmitryvs20170@gmail.com>
2023-09-04 21:22:37 +0500

    commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+const { URL } = require("url");
+const http = require("http");
+const getUsers = require("./modules/users");
+
+const server = http.createServer((request, response) => {
+  const url = new URL(request.url, "http://127.0.0.1:3003");
+  if (request.url === "/users") {
+    response.status = 200;
+    response.statusMessage = "OK";
+    response.header = "Content-Type: application/json";
+    response.write(getUsers());
+    response.end();
+    return;
+  }
+  if (request.url === "/?hello=dima") {
+    const helloValue = url.searchParams.get("hello");
+    response.status = 200;
+    response.statusMessage = "OK";
+    response.header = "Content-Type: text/html";
+    response.write(`Hello, ${helloValue}`);
+    response.end();
+    return;
+  }
+
+  if (request.url === "/?hello=") {
+    response.status = 400;
+    response.header = "Content-Type: text/html";
+    response.end("Enter a name");
+    return;
+  }
+
+  if (request.url !== "/?hello=") {
+    response.status = 500;
+    response.header = "Content-Type: text/html";
+    response.end("");
+    return;
+  }
+
+  response.status = 200;
+  response.statusMessage = "OK";
+  response.header = "Content-Type: text/plain";
+  response.write("Hello, World!");
+  response.end();
+});
+
+server.listen(3003, () => {
+  console.log("Сервер запущен по адресу http://127.0.0.1:3003");
+});
