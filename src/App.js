b51542e382ea787786d704a2be09930b16dc9c1a
DVS-675 <dmitryvs20170@gmail.com>
2023-09-05 21:21:38 +0500

    finished

--- src/App.js
+++ src/App.js
@@ -4,35 +4,38 @@ const getUsers = require("./modules/users");
 
 const server = http.createServer((request, response) => {
   const url = new URL(request.url, "http://127.0.0.1:3003");
-  if (request.url === "/users") {
-    response.status = 200;
-    response.statusMessage = "OK";
-    response.header = "Content-Type: application/json";
+
+  if (url.searchParams.has("users")) {
+    response.writeHead(200, { "Content-Type": "application/json" });
     response.write(getUsers());
     response.end();
     return;
   }
-  if (request.url === "/?hello=dima") {
-    const helloValue = url.searchParams.get("hello");
-    response.status = 200;
-    response.statusMessage = "OK";
-    response.header = "Content-Type: text/html";
-    response.write(`Hello, ${helloValue}`);
-    response.end();
 
-    return;
+  if (url.searchParams.has("hello")) {
+    if (url.searchParams.get("hello").length > 0) {
+      response.writeHead(200, { "Content-Type": "text/plain" });
+      response.write("Hello, " + url.searchParams.get("hello"));
+      response.end();
+      return;
+    } else {
+      response.write("Enter a name");
+      response.end();
+      return;
+    }
   }
 
-  if (request.url === "/?hello=") {
-    response.status = 400;
-    response.header = "Content-Type: text/html";
-    response.end("Enter a name");
-    return;
+  console.log(url.searchParams.keys());
+
+  for (const key of url.searchParams.keys()) {
+    if (key !== "name" || key !== "user") {
+      response.writeHead(500, { "Content-Type": "text/plain" });
+      response.end();
+      return;
+    }
   }
 
-  response.status = 200;
-  response.statusMessage = "OK";
-  response.header = "Content-Type: text/plain";
+  response.writeHead(200, { "Content-Type": "text/plain" });
   response.write("Hello, World!");
   response.end();
 });
